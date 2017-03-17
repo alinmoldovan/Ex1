569533a69699a7a0cc355e0730d68e66f64432e1
Alin M <alinmoldovanbmx@gmail.com>
2017-03-17 09:29:34 +0200

    Initial Commit

---
+++ Ex1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ex1
+//
+//  Created by Alin Moldovan on 17/03/17.
+//  Copyright © 2017 Alin Moldovan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
